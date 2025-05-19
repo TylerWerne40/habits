ae1997472fcd079db1c6ac264382e0fd897c0cc3
TylerWerne40 <TylerWerne40@gmail.com>
2025-05-19 04:43:04 -0600

    completing habits

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirt, url_for
+from collections import defaultdict
 import datetime
 app = Flask(__name__)
 
 habits = ["Test habit", "Test habit 2"]
+
+completions = defaultdict(list)
+
 
 @app.context_processor
 def add_calc_daterange():
@@ -21,6 +25,7 @@ def index():
     return render_template("index.html", 
         habits=habits, 
         title="Habit Tracker - Home", 
+        completions = completions[selected_date],
         selected_date = selected_date)
 
 
@@ -32,3 +37,11 @@ def add_habit():
     return render_template("add_habit.html", 
         title="Habit Tracker - Add Habit", 
         selected_date = datetime.date.today())
+
+@app.post("/complete"):
+    date_str = request.form.get("date")
+    habit = request.form.get("habitName")
+    date = datetime.date.fromisoformat(date_str)
+    completions[date].append(habit)
+    
+    return redirt(url_for("index", date=date_str))
